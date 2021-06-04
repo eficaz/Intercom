64bb3c62d8c5f198ff80837ad9d30214bb9af43e
divyaeficaz <divya.eficaz@gmail.com>
2021-06-04 10:25:11 +0530

    Intial commit

---
+++ Intercomapp/Intercomapp/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Intercomapp.Services;
+using Intercomapp.Views;
+
+namespace Intercomapp
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Intercomapp/Intercomapp/Views/AboutPage.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Intercomapp.Views
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+            ShowProgress(true);
+            if (myWebView.CanGoBack)
+                myWebView.GoBack();
+            else
+            {
+                entryURL.Text = "https://camppro.eficaztechsol.com/";
+                LoadWebPage();
+
+            }
+        }
+
+        private void EntryURL_Completed(object sender, EventArgs e)
+        {
+            //ShowProgress(true);
+            LoadWebPage();
+        }
+
+        private void MyWebView_Navigating(object sender, WebNavigatingEventArgs e)
+        {
+            ShowProgress(true);
+            entryURL.Text = e.Url.ToString();
+        }
+
+        private void BtnLoad_Clicked(object sender, EventArgs e)
+        {
+            LoadWebPage();
+        }
+
+        public void LoadWebPage()
+        {
+            ShowProgress(true);
+            if (entryURL.Text != null && !entryURL.Text.ToString().Trim().Equals(""))
+                myWebView.Source = new UriBuilder(entryURL.Text.ToString()).Uri;
+        }
+
+        private void MyWebView_Navigated(object sender, WebNavigatedEventArgs e)
+        {
+            entryURL.Text = e.Url.ToString();
+            ShowProgress(false);
+        }
+
+        public void ShowProgress(Boolean show)
+        {
+            activityIndicator.IsVisible = show;
+            activityIndicator.IsRunning = show;
+
+        }
+    }
+}
